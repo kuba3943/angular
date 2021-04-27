8adfa2614a8a792bc53623ac21f9e06cf78ad4c1
Jakub Domagała <kuba.domagala3@gmail.com>
2021-04-27 19:56:30 +0200

    show grades

--- src/app/show-grades/show-grades.component.ts
+++ src/app/show-grades/show-grades.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Grades } from '../models/grades';
+import { Point } from '../models/point';
 import { Student } from '../models/student';
 import { ClassService } from '../services/class.service';
+import { StudentService } from '../services/student.service';
 
 @Component({
   selector: 'app-show-grades',
@@ -10,19 +13,49 @@ import { ClassService } from '../services/class.service';
 })
 export class ShowGradesComponent implements OnInit {
 
-  constructor(private classService: ClassService, private route: ActivatedRoute,) { }
+
+  constructor(private studentService: StudentService, private classService: ClassService, private route: ActivatedRoute,) { }
 
 
   students!: Array<Student>;
 
 
 classId: any = this.route.snapshot.paramMap.get('classId');
+subjectId: any = this.route.snapshot.paramMap.get('subjectId');
+
+avgValue!: any;
+
+grades!: Grades;
+
+id: any;
+
+  a: number = 1;
+
+points2: Array<Point> = [];
+
+point:Point = new Point;
+
 
 
   ngOnInit(): void {
     this.showStudents(this.route.snapshot.paramMap.get('classId'));
+    this.getPoints();
   }
 
+  getPoints(){
+    this.studentService.getPoints().subscribe(data => {console.log(data); this.points2 = data});
+  }
+
+  addPoint(studentId:any, subjectId:any, pointId:any){
+    this.studentService.addPointToStudent(studentId, subjectId, pointId).subscribe(data => {console.log(data); });
+    this.point = new Point;
+  }
+
+  studentID(studentid: any){
+
+    this.id = studentid*this.a;
+    this.a = this.a* (-1);
+  }
 
   showStudents(id:any): void {
     this.classService.getStudents(id)
@@ -35,4 +68,25 @@ classId: any = this.route.snapshot.paramMap.get('classId');
           console.log(error);
         });
   }
+
+  getGrades(studentId:any, subjectId:any){
+    this.studentService.getGrades(studentId,subjectId).subscribe(data => {this.grades = data;  this.avg(data)});
+
+  }
+
+
+  avg(grades:Grades){
+
+    if (grades.points === null){
+      this.avgValue = 0;
+    } else {
+    const sum = this.grades.points.reduce((a, b) => a + b.point, 0);
+    const avg = (sum / this.grades.points.length) || 0;
+
+    this.avgValue = avg; }
+  }
+
+
+
+
 }
